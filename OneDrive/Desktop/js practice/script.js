510bb88320e4c940d41c3691ec02f33da5f22b8f
varshini-student <mvarshini1708@gmail.com>
2026-05-10 10:30:00 +0530

    Added reamaning files

---
+++ OneDrive/Desktop/js practice/script.js
@@ -0,0 +1,5 @@
+import {pi,add} from "./script1.js"
+
+console.log(pi)
+console.log(add(1,2))
+console.log(sub(1,2))
---
+++ OneDrive/Desktop/js practice/script1.js
@@ -0,0 +1,7 @@
+export const pi=3.14
+export function add(a,b){
+    return a+b
+}
+export function sub(a,b){
+    return a-b
+}
